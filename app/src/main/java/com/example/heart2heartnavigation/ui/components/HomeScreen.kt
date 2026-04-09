a0de495097ec3c81c13c4a1b9d5c18840dbffc8e
mohsen <mohsen.estakhr123@gmail.com>
2026-04-09 17:36:23 +0200

    ss

--- app/src/main/java/com/example/heart2heartnavigation/ui/components/HomeScreen.kt
+++ app/src/main/java/com/example/heart2heartnavigation/ui/components/HomeScreen.kt
@@ -2,7 +2,9 @@ package com.example.heart2heartnavigation.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -14,7 +16,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Battery6Bar
+import androidx.compose.material.icons.outlined.Bluetooth
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.heart2heartnavigation.R
 import com.example.heart2heartnavigation.ui.components.H2HButton
 import com.example.heart2heartnavigation.ui.components.H2HDivider
@@ -70,9 +77,57 @@ fun HomeScreen(
 
 
             )
+            Spacer(modifier = Modifier
 
-            Spacer(modifier = Modifier.width(9.dp))
-            Spacer(modifier = Modifier.height(20.dp))
+                .offset(y = (-20).dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(6.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.Bluetooth,
+                        contentDescription = "Bluetooth",
+                        tint = pink,
+                        modifier = Modifier.size(22.dp)
+                            .offset(y = (-140).dp)
+
+                    )
+
+                    Text(
+
+                        text = "Connected",
+                        fontSize = 14.sp,
+                        modifier = Modifier
+                            .offset(y = (-140).dp)
+                    )
+
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(0.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.Battery6Bar,
+                        contentDescription = "Battery",
+                        tint = pink,
+                        modifier = Modifier.size(22.dp)
+                            .offset(y = (-140).dp)
+                    )
+
+                    Text(
+                        text = "64%",
+                        fontSize = 14.sp,
+                        modifier = Modifier
+                            .offset(y = (-140).dp)
+                    )
+                }
+            }
 
             H2HButton(
                 text = "Disconnect",
